68937dd0b1fcf12ee4ef2f16193491479366477c
SnippetTechie <mohammedtahir0099@gmail.com>
2025-11-14 22:44:10 +0530

    Initial frontend structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>DevLance</h1>
+        <p>A decentralized freelance platform that securely connects external clients with our club’s developers</p>
+      </div>
+    </>
+  )
+}
+
+export default App
