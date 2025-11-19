076dc538c0f390fc06b84c1a7291f1010c2d6213
harpreet-2146 <harpreetk2146@gmail.com>
2025-11-19 06:44:32 +0530

    pages-task details added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Wallet from "./pages/Wallet";
 import Home from './pages/Home';
+import TaskDetail from "./pages/TaskDetail";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} /> 
-        <Route path="/wallet" element={<Wallet />} /> 
+        <Route path="/wallet" element={<Wallet />} />
+        <Route path="/task/:id" element={<TaskDetail />} /> 
       </Routes>
     </Router>
   );
